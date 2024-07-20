74b7a5e00f8a921587e8303931153d3b72082e51
libyao <1052433260@qq.com>
2024-07-21 00:19:25 +0800

    update

--- main.c
+++ main.c
@@ -1,10 +1,9 @@
-#include  <stdio.h>
+#include <stdio.h>
 #include <stdlib.h>
 
 #include "C_primer_plus/head.h"
 
-int main(void)
-{
+int main(void) {
 #if defined(WIN32)
     printf("*** running from win32 platform ***\n");
     system("chcp 65001");
